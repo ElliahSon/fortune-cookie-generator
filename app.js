68cd8aef7546458adf5ab920690b075c96c5bd7b
ElliahSon <30949066+ElliahSon@users.noreply.github.com>
2018-02-23 14:12:53 -1000

    rhsrtn

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-
 var fortunesList = [
   "You'll find a $100 bill on the sidewalk.",
   "You will win a random lottery.",
   "You will find a four leafed clover.",
   "A tree will fall on your neighbors car.",
   "You will find 100 pennies in a span of 30 days.",
-  "The next fortune will feel more rewarding after this",
+  "The next fortune will feel more rewarding after this.",
+  "You will wake up with the power to shut anyones mouth for 5 min.",
+  "You are able to control how much and how powerful your farts are.",
+  "You will wake up with a full beard"
 ];
 
 
@@ -15,16 +17,24 @@ function generateFortuneCookie () {
 
   document.getElementById("fortune-cookie-text").innerHTML = fortunesList[rand];
 
-  var list = document.createElement("LI");
+  var list = document.createElement("li");
 
-  var text = document.createTextNode("fortuneList[rand]");
+  var text = document.createTextNode(fortunesList[rand]);
 
   list.appendChild(text);
 
   document.getElementById("previous-fortunes").appendChild(list);
   
-  var prev = document.getElementById("previous-fortunes");
-  
-  prev.insertBefore(List, prev.childNodes[0]);
-  
+  fortunesList.splice(rand, 1);
+
+  if (fortunesList.length == 0) {
+    fortunesList.push("You'll find a $100 bill on the sidewalk.",
+  "You will win a random lottery.",
+  "You will find a four leafed clover.",
+  "A tree will fall on your neighbors car.",
+  "You will find 100 pennies in a span of 30 days.",
+  "The next fortune will feel more rewarding after this","You will wake up with the power to shut anyones mouth for 5 min.",
+  "You are able to control how much and how powerful your farts are.",
+  "You will wake up with a full beard");
+  }
 }
